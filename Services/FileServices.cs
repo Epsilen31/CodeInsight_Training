ef0ec216a7b0547f79a170d79ed81103e982d0f6
Abhishek <abhishek.mishra@thecodeinsight.com>
2025-01-14 11:36:32 +0530

    changed the naming convention in the FileServices.cs

--- Services/FileServices.cs
+++ Services/FileServices.cs
@@ -10,22 +10,18 @@ namespace Codeinsight.FileManager.Services
         //private const string beforeMoveddestinationPath = "E:\\C#\\FileHandler\\FileHandler\\Services\\copyText.csv";
         //private const string afterMovedsourcePath = "E:\\C#\\FileHandler\\FileHandler\\Contracts\\copyText.csv";
 
-
         //public string FilePath { get => filePath; }
         //public string BeforeMoveddestinationPath { get => beforeMoveddestinationPath; }
         //public string AfterMovedsourcePath { get => afterMovedsourcePath; }
-
-
+    
         public void FileImplimentatiionTask()
         {
-            const string filePath = "E:\\C#\\FileHandler\\Codeinsight.FileManager\\Codeinsight.FileManager\\Contracts\\Text.csv";
-            //const string beforeMoveddestinationPath = "E:\\C#\\FileHandler\\FileHandler\\Services\\copyText.csv";
-            //const string afterMovedsourcePath = "E:\\C#\\FileHandler\\FileHandler\\Contracts\\copyText.csv";
+            const string FilePath = "E:\\C#\\FileHandler\\Codeinsight.FileManager\\Codeinsight.FileManager\\Contracts\\Text.csv";
+            const string BeforeMoveddestinationPath = "E:\\C#\\FileHandler\\FileHandler\\Services\\copyText.csv";
+            const string AfterMovedsourcePath = "E:\\C#\\FileHandler\\FileHandler\\Contracts\\copyText.csv";
 
-
-            string ReadContent = fileProcess.ReadFile(filePath);
+            string ReadContent = fileProcess.ReadFile(FilePath);
             Console.WriteLine(ReadContent);
-
 
             //Method 1
             // Console.WriteLine("Enter Something Here to Add...");
@@ -33,15 +29,13 @@ namespace Codeinsight.FileManager.Services
             //string writContent = fileProcess.WriteFile(FilePath, userInput ?? string.Empty);
 
             //Method 2
-            //fileProcess.WriteFile(FilePath, "Hello Abhishek twice, 332");
+            fileProcess.WriteFile(FilePath, "Hello Abhishek twice, 332");
 
-            //fileProcess.DeleteFile(BeforeMoveddestinationPath);
+            fileProcess.DeleteFile(BeforeMoveddestinationPath);
 
-            //fileProcess.CopyFile(FilePath, BeforeMoveddestinationPath);
+            fileProcess.CopyFile(FilePath, BeforeMoveddestinationPath);
 
-            //fileProcess.MoveFile(BeforeMoveddestinationPath, AfterMovedsourcePath);
-
+            fileProcess.MoveFile(BeforeMoveddestinationPath, AfterMovedsourcePath);
         }
-
     }
 }
