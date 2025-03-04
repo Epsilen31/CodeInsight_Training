32ff5e1e8fdb055f407be4c85cfdfef916e7736d
Abhishek <abhishek.mishra@thecodeinsight.com>
2025-03-04 18:58:08 +0530

    update

---
+++ BillingAndSubscriptionSystem/EntityFrameWork/BillingAndSubscriptionSystem.Entities/Entities/Menu.cs
@@ -0,0 +1,13 @@
+namespace BillingAndSubscriptionSystem.Entities.Entities
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Path { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+
+        public string Role { get; set; } = "Admin";
+        public ICollection<SubMenu> SubMenus { get; set; } = [];
+    }
+}
